970f30ecf2937bef7ba764a110f8f24c6788322f
Stellce <andrey.is323@gmail.com>
2023-08-05 12:15:48 +0200

    initial commit, angular material added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MenComponent} from "./men/men.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'men', component: MenComponent},
+  {path: '', redirectTo: 'men', pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
