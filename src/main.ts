b1ed61803d89469f530873913f3d2b4da719bca4
Stellce <andrey.is323@gmail.com>
2024-10-13 15:49:11 +0200

    SSR enabled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
 import { AuthInterceptor } from './app/auth/auth.interceptor';
 import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import {provideRouter} from "@angular/router";
@@ -17,7 +15,7 @@ bootstrapApplication(AppComponent, {
         { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
         provideAnimations(),
         provideHttpClient(withInterceptorsFromDi()),
-        provideRouter(routes)
+        provideRouter(routes), provideClientHydration()
     ]
 })
   .catch(err => console.error(err));
