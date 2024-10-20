2db0ac8bba97184ac3871a7f5db82471b49bd564
Stellce <andrey.is323@gmail.com>
2024-10-20 12:25:51 +0200

    Reactive Register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,48 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
+import {ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 import {ActivatedRoute, RouterLink} from '@angular/router';
-import { AuthService } from "../auth.service";
-import { RegisterUser } from "./register-user.model";
+import {AuthService} from "../auth.service";
+import {RegisterUser} from "./register-user.model";
 import {GoogleLoginButtonComponent} from "../google-login-button/google-login-button.component";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @Component({
-    selector: 'app-register',
-    templateUrl: './register.component.html',
-    styleUrls: ['../shared.scss'],
-    standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatCheckboxModule, MatButtonModule, RouterLink, GoogleLoginButtonComponent, MatProgressSpinner]
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['../shared.scss'],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatCheckboxModule, MatButtonModule, RouterLink, GoogleLoginButtonComponent, MatProgressSpinner],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterComponent implements OnInit {
-  form: FormGroup;
-  showError: boolean = false;
-  showEmailResend: boolean = true;
-  emailResendTimer:number = 0;
-  isLoading: boolean = false;
+  protected readonly form: FormGroup = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    isAgreementConsent: new FormControl('', Validators.required)
+  });
+  protected readonly showError: WritableSignal<boolean> = signal<boolean>(false);
+  protected readonly showEmailResend: WritableSignal<boolean> = signal<boolean>(true);
+  protected readonly emailResendTimer: WritableSignal<number> = signal<number>(0);
+  protected readonly isLoading: WritableSignal<boolean> = signal<boolean>(false);
 
-  constructor(public authService: AuthService, private route: ActivatedRoute) {}
+  constructor(protected authService: AuthService, private route: ActivatedRoute) {}
+
   ngOnInit() {
-    this.form = new FormGroup({
-      firstname: new FormControl('', Validators.required),
-      lastname: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required),
-      isAgreementConsent: new FormControl('', Validators.required)
-    });
-
-    const code = this.route.snapshot.queryParamMap.get('code');
-    if(this.authService.user() || !code) return;
-    this.isLoading = true;
-    this.authService.loginGoogle(code);
+    this.checkGoogleAuth();
   }
 
   onRegister() {
-    this.showError = true;
+    this.showError.set(true);
     if(this.form.invalid) return;
     let registerUser: RegisterUser = {
       firstname: this.form.value.firstname,
@@ -52,8 +49,8 @@ export class RegisterComponent implements OnInit {
     }
     console.log(registerUser);
     this.authService.register(registerUser).subscribe({
-      next: () => this.showEmailResend = true,
-      error: () => this.showEmailResend = true
+      next: () => this.showEmailResend.set(true),
+      error: () => this.showEmailResend.set(true)
     });
   }
 
@@ -62,22 +59,29 @@ export class RegisterComponent implements OnInit {
       next: () => {
         if (this.emailResendTimer) return;
 
-        this.emailResendTimer = 15;
-        this.showEmailResend = false;
+        this.emailResendTimer.set(15);
+        this.showEmailResend.set(false);
 
         let interval = setInterval(() => {
           if (this.emailResendTimer) {
-            this.emailResendTimer--;
+            this.emailResendTimer.update(time => time--);
           } else {
             clearInterval(interval);
-            this.showEmailResend = true;
+            this.showEmailResend.set(true);
           }
         }, 1000);
       },
       error: () => {
-        this.showEmailResend = true;
-        this.emailResendTimer = 0;
+        this.showEmailResend.set(true);
+        this.emailResendTimer.set(0);
       }
     });
+  }
+
+  private checkGoogleAuth() {
+    const code = this.route.snapshot.queryParamMap.get('code');
+    if(this.authService.user() || !code) return;
+    this.isLoading.set(true);
+    this.authService.loginGoogle(code);
   }
 }
