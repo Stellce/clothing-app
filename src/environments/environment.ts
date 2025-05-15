794d0d1f02ea8d4fdbc102d9fd2ebd649871826c
Stellce <andrey.is323@gmail.com>
2025-05-15 21:25:57 +0200

    Docker, nginx configuration added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // backendUrl: 'http://localhost:3000',
-  backendUrl: 'http://api-gateway:8765'
+  backendUrl: 'http://localhost:8765',
+  // backendUrl: 'http://nginx:80'
 }
